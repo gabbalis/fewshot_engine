85aedf4eea3600cf7f1e1d7c77159b0a1095a902
gabbalis <nicholascraycraft@gmail.com>
2023-09-03 15:40:49 -0400

    add sample utility fn

---
+++ sample_util.py
@@ -0,0 +1,25 @@
+from few_shot_engine.gui import Gui
+from few_shot_engine.few_shot_engine import FewShotEngine
+
+gui = Gui()
+
+def utility_function(name, save_directory, input_string, num_examples=3, save_examples=False, base_prompt=""):
+    # Step 1: Create a new FewShotEngine
+    engine = FewShotEngine(name, save_directory, num_examples, save_examples, base_prompt)
+
+    # Step 2: Zip the files
+    engine.zip()
+
+    # Step 3: Process
+    engine.process(input_string)  # Passing the input_string here
+
+    # Step 4: Unzip
+    engine.unzip()
+
+    # Step 5: Get last generated directories
+    dirs = engine.get_last_gen_dirs()
+    if dirs:
+        metadata_dir, input_dir, output_dir = dirs  # Adjust depending on your actual structure
+
+        # Step 6: Send to GUI
+        gui.display_fewshot(metadata_dir, input_dir, output_dir)
