8e58135a0b9e456dd3f40408dbc61f8f3489c133
gabbalis <nicholascraycraft@gmail.com>
2023-09-03 15:45:44 -0400

    add return, save by default

--- sample_util.py
+++ sample_util.py
@@ -3,7 +3,7 @@ from few_shot_engine.few_shot_engine import FewShotEngine
 
 gui = Gui()
 
-def utility_function(name, input_string, save_directory='.', num_examples=3, save_examples=False, base_prompt=""):
+def utility_function(name, input_string, save_directory='.', num_examples=3, save_examples=True, base_prompt=""):
     # Step 1: Create a new FewShotEngine
     engine = FewShotEngine(name, save_directory, num_examples, save_examples, base_prompt)
 
@@ -11,7 +11,7 @@ def utility_function(name, input_string, save_directory='.', num_examples=3, sav
     engine.zip()
 
     # Step 3: Process
-    engine.process(input_string)  # Passing the input_string here
+    output = engine.process(input_string)  # Passing the input_string here
 
     # Step 4: Unzip
     engine.unzip()
@@ -23,3 +23,5 @@ def utility_function(name, input_string, save_directory='.', num_examples=3, sav
 
         # Step 6: Send to GUI
         gui.display_fewshot(metadata_dir, input_dir, output_dir)
+
+    return output
